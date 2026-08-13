9419404bb65295b88d9d3929095d117ffe765491
Samuel Freitas <samul.moreira2006@gmail.com>
2026-08-13 09:32:20 -0300

    feat: cria estrutura de pacotes e classes Carro e Sistema

---
+++ src/br/com/meusistema/model/Carro.java
@@ -0,0 +1,10 @@
+package br.com.meusistema.model;
+
+public class Carro {
+    public String modelo;
+    public String cor;
+
+    public void buzinar() {
+        System.out.println("O " + modelo + " " + cor + " fez: BIIII BIIII!");
+    }
+}
